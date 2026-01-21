e4ae387ae07bd026befe928a5241ad157fb9ef79
Mourchid FOLARIN <mourchidolawale@gmail.com>
2026-01-21 18:08:04 +0100

    Fix: Force HTTPS for secure forms

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Carbon\Carbon; // <--- N'oublie pas d'ajouter cet import
+use Carbon\Carbon; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,15 +19,20 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        // On force Carbon (les dates) à parler français
+  public function boot(): void
+{   // On force Carbon (les dates) à parler français
         Carbon::setLocale('fr');
         
         // Optionnel : On force aussi PHP pour certains formats spécifiques
         setlocale(LC_TIME, 'fr_FR.utf8');
         Paginator::useTailwind();
+    // Force le HTTPS si l'application est en production sur Railway
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
     }
+}
+
+    
 
   
 }
