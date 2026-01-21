6cca31028e83be1cfb7683c3de6620fa30752506
Mourchid FOLARIN <mourchidolawale@gmail.com>
2026-01-21 19:59:59 +0100

    non

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,22 +1,7 @@
 <div>
-    <p class="mt-6 text-center text-sm  flex">
-
-        <a href="{{ route('home') }}" class="text-gray-400 font-medium flex justify-center items-center gap-3 ">
-            Retour a l'Accueil
-        </a>
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button type="submit" class="text-gray-500 hover:text-red-700">
-                Deconnexion
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"
-                    class="icon icon-tabler icons-tabler-outline icon-tabler-logout-2">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
-                    <path d="M15 12h-12l3 -3" />
-                    <path d="M6 15l-3 -3" />
-                </svg>
-            </button>
-        </form>
+   <p class="mt-6 text-center text-sm ">
+            <a href="{{route('home')}}" class="text-gray-400 font-medium flex justify-center items-center gap-3 ">
+                Retour a l'Accueil
+            </a>
     </p>
 </div>
