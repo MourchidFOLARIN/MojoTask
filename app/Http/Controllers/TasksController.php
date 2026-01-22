7ee6afdff9935f939ba8d6cd082eb86954fc8473
Mourchid FOLARIN <mourchidolawale@gmail.com>
2026-01-22 11:13:30 +0100

    Fix AppServiceProvider and force config clear

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 use App\Models\Task;
 use App\Models\User;
 use App\Models\TaskReminder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TasksController extends Controller
 {
@@ -117,12 +117,9 @@ class TasksController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
-        return redirect('/');
+        return to_route('home');
     }
     
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Providers;
+
+use Carbon\Carbon;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\ServiceProvider;
-use Carbon\Carbon; 
+use Illuminate\Pagination\Paginator; // Corrigé ici (pas de 'n')
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\ServiceProvider; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,20 +21,23 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-  public function boot(): void
-{   // On force Carbon (les dates) à parler français
+    public function boot(): void
+    {
+        // On force Carbon (les dates) à parler français
         Carbon::setLocale('fr');
         
-        // Optionnel : On force aussi PHP pour certains formats spécifiques
+        // On force le nettoyage du cache à chaque démarrage
+        Artisan::call('config:clear');
+
+        // On force PHP pour certains formats spécifiques
         setlocale(LC_TIME, 'fr_FR.utf8');
+        
+        // Utilisation de Tailwind pour la pagination
         Paginator::useTailwind();
-    // Force le HTTPS si l'application est en production sur Railway
-    if (config('app.env') === 'production') {
-        URL::forceScheme('https');
+
+        // Force le HTTPS si l'application est en production sur Railway
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
-}
-
-    
-
-  
 }
